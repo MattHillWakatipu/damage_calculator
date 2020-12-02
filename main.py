98815a7dcc4e37bb64fda966544dd6157f0621df
Matt <matthillwakatipu@gmail.com>
2020-12-02 17:38:31 +1300

    Fresh and spent encounter sim complete

--- main.py
+++ main.py
@@ -52,13 +52,25 @@ def longsword_attack(ac):
     """
     attack_roll = roll_die(20) + 8
 
-    if attack_roll >= 28:
+    if attack_roll == 28:
         return roll_die(8) + roll_die(8) + 5
 
     if attack_roll >= ac:
         return roll_die(8) + 5
 
     return 0
+
+
+def expend_superiority_dice():
+    """
+    Expend all superiority dice for level 5 fighter.
+    :return: Total damage caused by superiority dice.
+    """
+    total_damage = 0
+    for x in range(4):
+        total_damage += roll_die(8)
+
+    return total_damage
 
 
 def emei_attack(ac):
@@ -95,54 +107,174 @@ def unarmed_attack(ac):
     return 0
 
 
-def abika_dpr(ac):
-    normal = sharpshooter_attack(ac) + sharpshooter_attack(ac)
-    burst = sharpshooter_attack(ac) + sharpshooter_attack(ac) + snipe_attack(ac)
-
-    return normal, burst
-
-
-def syra_dpr(ac):
-    normal = longsword_attack(ac) + longsword_attack(ac) + longsword_attack(ac)
-    burst = longsword_attack(ac) + longsword_attack(ac) + longsword_attack(ac) + longsword_attack(ac) + longsword_attack(ac)
-
-    return normal, burst
+def abika_normal(ac):
+    """
+    Full turn of Abika at level 5.
+    :param ac: AC of target creature.
+    :return: Damage caused by full turn.
+    """
+    return sharpshooter_attack(ac) + sharpshooter_attack(ac)
 
 
-def hazel_dpr(ac):
-    normal = emei_attack(ac) + emei_attack(ac) + unarmed_attack(ac)
-    burst = emei_attack(ac) + emei_attack(ac) + unarmed_attack(ac) + unarmed_attack(ac)
+def abika_burst(ac):
+    """
+    Full burst turn of Abika at level 5.
+    :param ac: AC of target creature.
+    :return: Damage caused by full turn.
+    """
+    return sharpshooter_attack(ac) + sharpshooter_attack(ac) + snipe_attack(ac)
 
-    return normal, burst
+
+def abika_fresh(ac):
+    """
+    Calculate damage caused over 5 rounds of combat, starting from fresh.
+    :param ac: AC of target creature.
+    :return: Damage caused during combat.
+    """
+    total_damage = 0
+
+    for x in range(4):
+        total_damage += abika_burst(ac)
+
+    total_damage += abika_normal(ac)
+
+    return total_damage
+
+
+def abika_spent(ac):
+    """
+    Calculate damage caused over 5 rounds of combat, starting from spent.
+    :param ac: AC of target creature.
+    :return: Damage caused during combat.
+    """
+    total_damage = 0
+
+    for x in range(5):
+        total_damage += abika_normal(ac)
+
+    return total_damage
+
+
+def syra_normal(ac):
+    """
+    Full turn of Syra at level 5.
+    :param ac: AC of target creature.
+    :return: Damage caused by full turn.
+    """
+    return longsword_attack(ac) + longsword_attack(ac) + longsword_attack(ac)
+
+
+def syra_burst(ac):
+    """
+    Full burst turn of Syra at level 5.
+    :param ac: AC of target creature.
+    :return: Damage caused by full turn.
+    """
+    return longsword_attack(ac) + longsword_attack(ac) + longsword_attack(ac) + longsword_attack(ac) + longsword_attack(ac)
+
+
+def syra_fresh(ac):
+    """
+    Calculate damage caused over 5 rounds of combat, starting from fresh.
+    :param ac: AC of target creature.
+    :return: Damage caused during combat.
+    """
+    total_damage = syra_burst(ac)
+
+    for x in range(4):
+        total_damage += syra_normal(ac)
+
+    total_damage + expend_superiority_dice()
+
+    return total_damage
+
+
+def syra_spent(ac):
+    """
+    Calculate damage caused over 5 rounds of combat, starting from spent.
+    :param ac: AC of target creature.
+    :return: Damage caused during combat.
+    """
+    total_damage = 0
+
+    for x in range(5):
+        total_damage += syra_normal(ac)
+
+    return total_damage
+
+
+def hazel_normal(ac):
+    """
+    Full turn of Hazel at level 5.
+    :param ac: AC of target creature.
+    :return: Damage caused by full turn.
+    """
+    return emei_attack(ac) + emei_attack(ac) + unarmed_attack(ac)
+
+
+def hazel_burst(ac):
+    """
+    Full burst turn of Hazel at level 5.
+    :param ac: AC of target creature.
+    :return: Damage caused by full turn.
+    """
+    return emei_attack(ac) + emei_attack(ac) + unarmed_attack(ac) + unarmed_attack(ac)
+
+
+def hazel_fresh(ac):
+    """
+    Calculate damage caused over 5 rounds of combat, starting from fresh.
+    :param ac: AC of target creature.
+    :return: Damage caused during combat.
+    """
+    total_damage = 0
+
+    for x in range(5):
+        total_damage += hazel_burst(ac)
+
+    return total_damage
+
+
+def hazel_spent(ac):
+    """
+    Calculate damage caused over 5 rounds of combat, starting from spent.
+    :param ac: AC of target creature.
+    :return: Damage caused during combat.
+    """
+    total_damage = 0
+
+    for x in range(5):
+        total_damage += hazel_normal(ac)
+
+    return total_damage
 
 
 if __name__ == '__main__':
 
-    abika_normal, abika_burst, syra_normal, syra_burst, hazel_normal, hazel_burst = 0, 0, 0, 0, 0, 0
-    trials = 100000
+    a_fresh, a_spent, s_fresh, s_spent, h_fresh, h_spent = 0, 0, 0, 0, 0, 0
+    trials = 10000
 
-    for ac in range(10, 25):
+    for ac in range(10, 26):
         for n in range(trials):
 
-            a_normal, a_burst = abika_dpr(ac)
-            s_normal, s_burst = syra_dpr(ac)
-            h_normal, h_burst = hazel_dpr(ac)
+            a_fresh += abika_fresh(ac)
+            a_spent += abika_spent(ac)
+            h_fresh += hazel_fresh(ac)
+            h_spent += hazel_spent(ac)
+            s_fresh += syra_fresh(ac)
+            s_spent += syra_spent(ac)
 
-            abika_normal += a_normal
-            abika_burst += a_burst
-            syra_normal += s_normal
-            syra_burst += s_burst
-            hazel_normal += h_normal
-            hazel_burst += h_burst
-
-        abika_normal /= trials
-        abika_burst /= trials
-        syra_normal /= trials
-        syra_burst /= trials
-        hazel_normal /= trials
-        hazel_burst /= trials
+        a_fresh /= trials
+        a_spent /= trials
+        h_fresh /= trials
+        h_spent /= trials
+        s_fresh /= trials
+        s_spent /= trials
 
         print("AC:", ac)
-        print("Abika, Normal: % .2f, Burst: % .2f" % (abika_normal, abika_burst))
-        print("Syra,  Normal: % .2f, Burst: % .2f" % (syra_normal, syra_burst))
-        print("Hazel, Normal: % .2f, Burst: % .2f" % (hazel_normal, hazel_burst))
+        print("Abika, Fresh: %.2f, Spent: %.2f, Combined: %.2f" %
+              (a_fresh, a_spent, a_fresh + a_spent))
+        print("Syra,  Fresh: %.2f, Spent: %.2f, Combined: %.2f" %
+              (s_fresh, s_spent, s_fresh + s_spent))
+        print("Hazel, Fresh: %.2f, Spent: %.2f, Combined: %.2f" %
+              (h_fresh, h_spent, h_fresh + h_spent))
