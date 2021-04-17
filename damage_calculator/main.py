95ff0d1b30c7c46d39864db37628b9a9a92c725b
Matt <50199412+MattHillWakatipu@users.noreply.github.com>
2021-04-17 23:00:08 +1200

    Create plots for Hazel's turns

--- damage_calculator/main.py
+++ damage_calculator/main.py
@@ -1,14 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from classes.monk import Monk
+
 if __name__ == '__main__':
-    x = np.linspace(0, 2, 100)
+    hazel = Monk(dexterity=4, proficiency_bonus=3, equipment_bonus=0)
+    hazel_rested = []
+    hazel_spent = []
+
+    for i in range(10, 25):
+        hazel_rested.append(hazel.rested_turn(i))
+        hazel_spent.append(hazel.spent_turn(i))
+
+    print(hazel_rested)
+    print(hazel_spent)
 
     # Note that even in the OO-style, we use `.pyplot.figure` to create the figure.
     fig, ax = plt.subplots()  # Create a figure and an axes.
-    ax.plot(x, x, label='linear')  # Plot some data on the axes.
-    ax.plot(x, x ** 2, label='quadratic')  # Plot more data on the axes...
-    ax.plot(x, x ** 3, label='cubic')  # ... and some more.
+    ax.plot(hazel_rested, label='Hazel Rested')
+    ax.plot(hazel_spent, label='Hazel Spent')
+    # ax.plot(x, x, label='linear')  # Plot some data on the axes.
+    # ax.plot(x, x ** 2, label='quadratic')  # Plot more data on the axes...
+    # ax.plot(x, x ** 3, label='cubic')  # ... and some more.
     ax.set_xlabel('x label')  # Add an x-label to the axes.
     ax.set_ylabel('y label')  # Add a y-label to the axes.
     ax.set_title("Simple Plot")  # Add a title to the axes.
